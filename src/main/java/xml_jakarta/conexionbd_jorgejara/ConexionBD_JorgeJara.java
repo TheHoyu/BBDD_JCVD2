ecbae16a9cd6103458c45615811baebd3d5da1b5
TheHoyu <jarablancojorge@gmail.com>
2023-12-05 11:56:06 +0100

    comentarios ++

--- src/main/java/xml_jakarta/conexionbd_jorgejara/ConexionBD_JorgeJara.java
+++ src/main/java/xml_jakarta/conexionbd_jorgejara/ConexionBD_JorgeJara.java
@@ -14,10 +14,15 @@ import java.util.Scanner;
 public class ConexionBD_JorgeJara {
 
     public static void main(String[] args) throws SQLException, ParseException {
-           Metodos met = new Metodos();
-           Scanner sc = new Scanner(System.in);
-           boolean continua=true;
-           System.out.println("Bienvenido a la base de datos JCVD");
+           Metodos met = new Metodos(); // instanciamos la clase metodos 
+           Scanner sc = new Scanner(System.in); // generamos escaner 
+           boolean continua=true; // boolean para continuar el menu hasta seleccionar salir 
+           
+           /*
+           Diseño de menu para la bbdd, con switch case 
+           */
+           
+           System.out.println("Bienvenido a la base de datos JCVD"); 
            System.out.println("-------------------------------------");
                System.out.println("1-buscaNombre por parametro ");
                System.out.println("2-lanzaConsulta por parametro");
@@ -27,27 +32,29 @@ public class ConexionBD_JorgeJara {
                System.out.println("6-salir");
                System.out.println("-------------------------------------");   
 
-        while (continua) {
+               
+        while (continua) { // bucle para usar el menu con variable booleana 
             System.out.println("-------------------------------------");
             System.out.print("Introduce un nº para elegir opcion: ");
 
-            int opcion = sc.nextInt();
+            int opcion = sc.nextInt(); // opcion para el menú 
             
             switch (opcion) {
                 case 1:
-                    met.buscaNombre("Carlos Duty");
+                    met.buscaNombre("Carlos Duty"); // metodoque busca juego por nombre por parámetro 
                     break;
 
-                case 2:
+                case 2: // metodo que genera una busqueda a traves de parámetro 
                     String consulta = "Select * from videojuegos where genero ='Drama social' ";
                     met.lanzaConsulta(consulta);
                     break;
 
-                case 3:
+                case 3: // uso del método que pide insertar un juego , por parámetro 
                     String nombre="Soldado Universal 37";
                     String genero="Drama social";
                     String FechaLanzamiento = "1994-04-19";
-                    SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+                    // me recalco y soy sincero, en el tema fechas chat gpt que me ha echado un cable 
+                    SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd"); 
                     Date fechaLanzamiento = dateFormat.parse(FechaLanzamiento);
                     java.sql.Date sqlFechaLanzamiento = new java.sql.Date(fechaLanzamiento.getTime());
                     String Compañia="El torete SL";
@@ -57,16 +64,16 @@ public class ConexionBD_JorgeJara {
                     break;
 
                 case 4:
-                    met.Insert();
+                    met.Insert(); // metodo que inserta un juego a través de teclado 
                     System.out.println("INSERCION CORRECTA");
                     break;
 
                 case 5:
-                    int eliminar = 13;
+                    int eliminar = 13; // método que elimina un juego pedidi por parámetro 
                     met.eliminarRegistro(eliminar);
                     break;
 
-                case 6:
+                case 6: // opcion de salir, rompemos la continuidad booleana, salimos del ciclo, vencemos a la matrix 
                     System.out.println("Saliendo de la base de datos");
                     continua = false;
                     break;
